2eaf41606c87712899d6efeca405b9c47ddfb7fe
xodud001 <freeptk1@gmail.com>
2021-05-19 23:24:05 +0900

    질문 게시글 및 댓글 객체 생성

---
+++ src/main/java/rev/team/BOARD_SERVICE/domain/entity/Ask.java
@@ -0,0 +1,43 @@
+package rev.team.BOARD_SERVICE.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "ask")
+public class Ask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(name = "ask_id")
+    private Long askId;
+
+    private String userId; // 작성자
+    private String nickname; // 작성자 작성 시 닉네임
+
+    @Column(columnDefinition = "TEXT")
+    private String title; // 제목
+
+    @Column(columnDefinition = "TEXT")
+    private String content; // 내용
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date postDate; // 작성일
+
+    @Column(columnDefinition = "integer default 0")
+    private Integer hits = 0; // 조회수
+
+    @Column(columnDefinition = "integer default 0")
+    private Integer recommend = 0;
+}
